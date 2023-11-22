dcd51954aefbac7da337a94751e0650862c8957d
programmer-for-me <programmer.for.me@gmail.com>
2023-11-22 18:41:19 +0500

    Done

--- app/src/main/java/com/example/mathgamebysam/model/Questions.kt
+++ app/src/main/java/com/example/mathgamebysam/model/Questions.kt
@@ -4,24 +4,25 @@ import kotlin.random.Random
 
 class Questions {
     var questions = mutableListOf<Pair<String, Int>>()
-    var answer = 0
     fun generateQuestions(sign: String) {
-        var num1 = Random.nextInt(-50, 50)
-        var num2 = Random.nextInt(-50, 50)
-        answer = when (sign) {
-            "+" -> num1 + num2
-            "-" -> num1 - num2
-            "*" -> num1 * num2
-            "/" -> num1 / num2
-            else -> num1 + num2
+        for(i in 0 .. 9) {
+            var num1 = Random.nextInt(-50, 50)
+            var num2 = Random.nextInt(-50, 50)
+            var answer = when (sign) {
+                "+" -> num1 + num2
+                "-" -> num1 - num2
+                "*" -> num1 * num2
+                "/" -> num1 / num2
+                else -> num1 + num2
+            }
+            questions.add(Pair("What is $num1 $sign $num2?", answer))
         }
-        questions.add(Pair("What is $num1 $sign $num2?", answer))
     }
     fun getQuestion(id: Int) : String {
         return questions[id].first
     }
     fun getOptions(id: Int) : MutableList<Int> {
-        var options = mutableListOf<Int>(
+        var options = mutableListOf(
             questions[id].second
         )
         options.add(questions[id].second + 2)
